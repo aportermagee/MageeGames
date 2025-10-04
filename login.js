4d94ad9ec171a33d5e7ecc0bf0f28d70d3b8e8b6
aportermagee <aportermagee@gmail.com>
2025-10-04 18:52:10 -0400

    Update login.js

--- login.js
+++ login.js
@@ -4,7 +4,7 @@ function submit(username, password) {
   } else {
     errorMessage.textContent = 'Incorrect username or password';
   }
-}
+};
 
 // Bypasses the Log-In page if the user is logged-in
 if (localStorage.getItem('loggedIn' === true)) {
@@ -33,4 +33,4 @@ let errorMessage = document.getElementById('errorMessage');
 
 submitBtn.addEventListener('click', function() {
   submit(username.value, password.value);
-})
+});
