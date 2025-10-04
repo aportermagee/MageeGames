b77da402366ca9769dcdf00b8f018238dd137d33
aportermagee <aportermagee@gmail.com>
2025-10-04 17:37:20 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,10 +1,17 @@
-users = {
-  "amagee": "apm2010!",
-};
+// Bypasses the Log-In page if the user is logged-in
+if (localStorage.getItem('loggedIn' === true)) {
+  window.location.href = 'home.html';
+}
 
+// Prevents the user from accessing the file
 document.addEventListener('contextmenu', event => event.preventDefault());
 document.addEventListener('keydown', function(event) {
   if (event.key === 'F12' || (event.ctrlKey && event.shiftKey && (event.key === 'I' || event.key === 'J'))) {
     event.preventDefault();
   }
 });
+
+// Usernames and passwords
+users = {
+  "amagee": "apm2010!",
+};
