efabb1338d69a045b9fa325e59a8ed8927eb9599
aportermagee <aportermagee@gmail.com>
2025-10-04 17:34:12 -0400

    Update login.js

--- login.js
+++ login.js
@@ -7,4 +7,4 @@ document.addEventListener('keydown', function(event) {
   if (event.key === 'F12' || (event.ctrlKey && event.shiftKey && (event.key === 'I' || event.key === 'J'))) {
     event.preventDefault();
   }
-}
+});
