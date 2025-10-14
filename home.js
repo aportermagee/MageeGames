4fa160ff29bd5c1b5d47bc4522c341fa12269c48
aportermagee <aportermagee@gmail.com>
2025-10-14 12:11:27 -0400

    Update home.js

--- home.js
+++ home.js
@@ -2,8 +2,12 @@ function isLoggedIn() {
   return (localStorage.getItem('loggedIn') === 'true');
 }
 
+function logout() {
+  localStorage.removeItem('loggedIn');
+}
+
 if (!(isLoggedIn())) {
   window.location.href = 'index.html';
 }
 
-const logoutBtn = document.getElementById("logoutBtn");
+const logoutBtn = document.getElementById('logoutBtn');
