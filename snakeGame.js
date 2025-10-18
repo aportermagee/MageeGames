d882abf3d8c5ad43677fb900c28fad3a9a3e584a
aportermagee <aportermagee@gmail.com>
2025-10-18 07:03:26 -0400

    Update snakeGame.js

--- snakeGame.js
+++ snakeGame.js
@@ -21,7 +21,7 @@ document.addEventListener('keydown', event => {
   if (event.key === 'ArrowRight' && direction !== 'LEFT') direction = 'RIGHT';
   if (event.key === 'ArrowUp' && direction !== 'DOWN') direction = 'UP';
   if (event.key === 'ArrowDown' && direction !== 'UP') direction = 'DOWN';
-};
+});
 
 function drawGame() {
   // Resart frame
