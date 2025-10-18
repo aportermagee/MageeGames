13bf29fd30331128f6c619d4b325c5eb5ae2488a
aportermagee <aportermagee@gmail.com>
2025-10-18 16:02:06 -0400

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -1,3 +1,9 @@
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+
+const box = 24;
+const speed = 200;
+
 const homeBtn = document.getElementById('homeBtn');
 
 homeBtn.addEventListener('click', function() {
