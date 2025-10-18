ad4c049dbca6d41ac2406b6b309a333f4a2d0998
aportermagee <aportermagee@gmail.com>
2025-10-18 16:34:49 -0400

    Update home.js

--- home.js
+++ home.js
@@ -1,25 +1,23 @@
-// Returns whether the user is logged in
-function isLoggedIn() {
-  return (localStorage.getItem('loggedIn') === 'true');
-}
-
-// Logs the user out and returns them to the login page
+// Logs out the user
 function logout() {
   localStorage.removeItem('loggedIn');
   window.location.href = 'index.html';
 }
 
-// Uses isLoggedIn to return user to the login page
+// Checks if the user is logged in
+function isLoggedIn() {
+  return (localStorage.getItem('loggedIn') === 'true');
+}
+
 if (!(isLoggedIn())) {
   window.location.href = 'index.html';
 }
 
-// Elements from the html file
+// Buttons
 const logoutBtn = document.getElementById('logoutBtn');
 const snakeGameBtn = document.getElementById('snakeGameBtn');
 const tetrisBtn = document.getElementById('tetrisBtn');
 
-// Checks whether the user has pressed the logout button
 logoutBtn.addEventListener('click', function() {
   logout();
 });
