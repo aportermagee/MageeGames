f1ceeb63d66f075f10c2e53032e864b0becc18f4
aportermagee <aportermagee@gmail.com>
2025-10-19 12:08:37 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,9 @@
 // --- Initialize Supabase ---
+import { createClient } from '@supabase/supabase-js';
+
 const SUPABASE_URL = 'https://crvmgootjfbqkokrwsuu.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNydm1nb290amZicWtva3J3c3V1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4MzkyNTQsImV4cCI6MjA3NjQxNTI1NH0.Em26tIW4z2ulfRePTOVhkCmcMGOa0OOjBqC3kPJ-LpU';
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 let currentUser = null;
 
