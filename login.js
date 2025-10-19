0a113109754976c2245b7540c52a4cc3179c6cf9
aportermagee <aportermagee@gmail.com>
2025-10-19 12:56:32 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 // --- Initialize Supabase ---
 const SUPABASE_URL = 'https://crvmgootjfbqkokrwsuu.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNydm1nb290amZicWtva3J3c3V1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4MzkyNTQsImV4cCI6MjA3NjQxNTI1NH0.Em26tIW4z2ulfRePTOVhkCmcMGOa0OOjBqC3kPJ-LpU';
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 let currentUser = null;
 
