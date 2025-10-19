29bfd1357e12ce8010f542d42927f38abc1ae934
aportermagee <aportermagee@gmail.com>
2025-10-19 11:58:23 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,36 +1,32 @@
-// Attempts to log in the user
-function submit(username, password) {
-  if (users[username] === password) {
-    localStorage.setItem('loggedIn', true);
-    localStorage.setItem('username', username);
-    window.location.href = 'home.html';
-  } else {
-    errorMessage.textContent = 'Incorrect username or password';
-  }
-}
+// --- Initialize Supabase ---
+const SUPABASE_URL = 'https://crvmgootjfbqkokrwsuu.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNydm1nb290amZicWtva3J3c3V1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4MzkyNTQsImV4cCI6MjA3NjQxNTI1NH0.Em26tIW4z2ulfRePTOVhkCmcMGOa0OOjBqC3kPJ-LpU';
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-// Checks whether the user is logged in
-function isLoggedIn() {
-  return (localStorage.getItem('loggedIn') === 'true');
-}
+let currentUser = null;
 
-if (isLoggedIn()) {
+// --- Sign Up ---
+document.getElementById('signup').addEventListener('click', async () => {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  const { data, error } = await supabase.auth.signUp({ email, password });
+  if (error) return alert('Error: ' + error.message);
+
+  alert('Sign up successful. Please check your email to confirm');
+});
+
+// --- Login ---
+document.getElementById('login').addEventListener('click', async () => {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+  if (error) return alert('Error: ' + error.message);
+
+  user = data.user;
+  
+  localStorage.setItem('user', user);
+  localStorage.setItem('loggedIn', true);
   window.location.href = 'home.html';
-}
-
-// Usernames and passwords
-const users = {
-  "amagee": "apm2010!",
-};
-
-// Html constants
-const username = document.getElementById('username');
-const password = document.getElementById('password');
-const errorMessage = document.getElementById('errorMessage');
-
-// Buttons
-const submitBtn = document.getElementById('submitBtn');
-
-submitBtn.addEventListener('click', function() {
-  submit(username.value, password.value);
 });
