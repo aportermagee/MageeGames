9612554dbdb31e5f2934f37437ede2adf724f35e
aportermagee <aportermagee@gmail.com>
2025-10-21 20:56:14 -0400

    Update spaceInvaders.js

--- spaceInvaders.js
+++ spaceInvaders.js
@@ -4,5 +4,5 @@ const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // Set up
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
