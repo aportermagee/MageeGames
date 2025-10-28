6fa0092ffb9fd32588093dc6b6e9767926869a54
aportermagee <aportermagee@gmail.com>
2025-10-28 17:56:19 -0400

    Update spaceInvaders.js

--- spaceInvaders.js
+++ spaceInvaders.js
@@ -18,13 +18,12 @@ const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
-const box = 12;
-const smallBox = 4;
+const box = 4;
 const speed = 20;
-const gameSpeed = 500 / speed;
+const gameSpeed = 160 / speed;
 const scoreP = document.getElementById('score');
 
-const playerY = canvas.height / box - 4;
+const playerY = canvas.height / box - 12;
 
 
 // ----- High Score -----
@@ -92,26 +91,26 @@ function start() {
 
   
   // --- Enemy ---
-  enemyY = 1;
+  enemyY = 3;
   enemyX = 0;
   enemyBullets = [];
   enemyDirection = 'right';
+  
+  enemies = [];
 
-  enemies = [
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-  ];
+  for (let x = 0; x < 4; x++) {
+    let temp = [];
+    for (let i = 0; i < 10; i++) {
+      temp.push(1);
+    }
+    enemies.push(temp);
+  }
 
   
   // Miscellaneous
   score = 0;
   round = 0;
-  gameTimer = Math.max(50, gameSpeed - round * 25);
+  gameTimer = Math.max(20, gameSpeed - round * 10);
   endGame = false;
   wall = false;
 
@@ -129,24 +128,24 @@ function newRound() {
   playerShotTimer = gameSpeed;
   
   // --- Enemy ---
-  enemyY = 1;
+  enemyY = 3;
   enemyX = 0;
   enemyBullets = [];
   enemyDirection = 'right';
+  
+  enemies = [];
 
-  enemies = [
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-  ];
-
+  for (let x = 0; x < 4; x++) {
+    let temp = [];
+    for (let i = 0; i < 10; i++) {
+      temp.push(1);
+    }
+    enemies.push(temp);
+  }
+  
   
   // Miscellaneous
-  gameTimer = Math.max(50, gameSpeed - round * 25);
+  gameTimer = Math.max(20, gameSpeed - round * 10);
   wall = false;  
 }
 
@@ -169,9 +168,11 @@ function moveLeft() {
 
 // --- Shoot ---
 function shoot() {
-  if (playerShotTimer < 1 && !(playerBullets.some(bullet => bullet[0] === playerX && bullet[1] === playerY - 1))) {
+  if (!playerBullets.some(bullet => ([playerX + x * 6, playerX + x * 6 + 1, playerX + x * 6 + 2].contains(bullet[0])) && 
+    ([playerY + y * 6 + 2, playerY + y * 6 + 3, playerY + y * 6 + 4].contains(bullet[1])))
+     ) {
     playerShotTimer = gameSpeed;
-    playerBullets.push([playerX, playerY - 1]);
+    playerBullets.push([playerX + 1, playerY - 2]);
   }
 }
 
@@ -192,6 +193,68 @@ document.addEventListener('keydown', event => {
   if (event.code === 'Space') shoot();
 });
 
+// ----- Collisions -----
+function playerBulletCollisions(i)  {
+  for (let x = enemyBullets.length - 1; x > -1; x--) {
+    if (
+      playerBullets[i][0] === enemyBullets[x][0] &&
+      [enemyBullets[x][1] - 1, enemyBullets[x][1], enemyBullets[x][1] + 1].contains(playerBullets[i][1])
+    ) {
+      playerBullets.splice(i, 1);
+      enemyBullets.splice(x, 1);
+      return true;
+    }
+  }
+  
+  for (let y = 0; y < enemies.length; y++) {
+    for (let x = 0; x < enemies[y].length; x++) {
+      if (enemies[y][x] === 1 &&
+          [enemyX + x * 6, enemyX + x * 6 + 1, enemyX + x * 6 + 2].contains(playerBullets[i][0]) &&
+          [enemyY + y * 6, enemyY + y * 6 + 1, enemyY + y * 6 + 2].contains(playerBullets[i][1])
+         ) {                                                                                                                                                                   
+        enemies[y][x] = 0;
+        playerBullets.splice(i, 1);
+        score += 10;
+        return true;
+      }
+    }
+  }
+  
+  if (playerBullets[i][1] + 1 < 0) {
+    playerBullets.splice(i, 1);
+    return true;
+  }
+
+  return false;
+}
+
+function enemyBulletCollisions(i) {
+  for (let x = playerBullets.length - 1; x > -1; x--) {
+    if (
+      enemyBullets[i][0] === playerBullets[x][0] &&
+      [playerBullets[x][1] - 1, playerBullets[x][1], playerBullets[x][1] + 1].contains(enemyBullets[i][1])
+    ) {
+      enemyBullets.splice(i, 1);
+      playerBullets.splice(x, 1);
+      return true;
+    }
+  }
+  
+  if ([playerX, playerX + 1, playerX + 2].contains(enemyBullets[i][0]) &&
+      [playerY, playerY + 1, playerY + 2].contains(enemyBullets[i][1])) {
+    health -= 1;
+    enemyBullets.splice(i, 1);
+    return true;
+  }
+
+  if (enemyBullets[i][1] > canvas.height / box - 1) {
+    enemyBullets.splice(i, 1);
+    return true;
+  }
+
+  return false;
+}
+
 
 // ----- Single Game Frame -----
 function drawFrame() {
@@ -204,18 +267,24 @@ function drawFrame() {
   // --- Bullet Movement ---
   if (gameTimer % 2 === 0) {
     for (let i = 0; i < enemyBullets.length; i++) {
-      enemyBullets[i][1] += 1;
+      for (let y = 0; y < 3; y++) {
+        enemyBullets[i][1] += 1;
+        if (enemyBulletCollisions(i)) break;
+      }
     }
   }
 
   for (let i = 0; i < playerBullets.length; i++) {
-    playerBullets[i][1] -= 1;
+    for (let y = 0; y < 3; y++) {
+      playerBullets[i][1] -= 1;
+      if (playerBulletCollisions(i)) break;
+    }
   }
 
        
   // --- Things to run every gameSpeed ---
   if (gameTimer < 1) {
-    gameTimer = Math.max(50, gameSpeed - round * 25);
+    gameTimer = Math.max(20, gameSpeed - round * 10);
     
     // --- Enemy --- 
     
@@ -224,7 +293,7 @@ function drawFrame() {
       for (let x = 0; x < enemies[y].length; x++) {  
         if (
           enemies[y][x] === 1 &&
-          (enemyX + x > canvas.width / box - 2 && enemyDirection === 'right' ||
+          (enemyX + x * 6 + 2 > canvas.width / box - 2 && enemyDirection === 'right' ||
           enemyX + x < 1 && enemyDirection === 'left')
         ) {
           wall = true
@@ -234,77 +303,24 @@ function drawFrame() {
 
     if (wall) {
       enemyDirection = (enemyDirection === 'right') ? 'left' : 'right';
-      enemyY += 1;
+      enemyY += 3;
       wall = false;
     } else {
       enemyX += (enemyDirection === 'right') ? 1 : -1;
     }
       
     
-    
     // Decides which enemies shoot
     for (let y = 0; y < enemies.length; y++) {
       for (let x = 0; x < enemies[y].length; x++) {
-        if ((enemies[y][x] === 1) && (Math.floor(Math.random() * 100) < 3 + round)) {
-          if (!enemyBullets.some(bullet => (bullet[0] === enemyX + x) && (bullet[1] === enemyY + y + 1))) {
-            enemyBullets.push([enemyX + x, enemyY + y + 1]);
+        if ((enemies[y][x] === 1) && (Math.round(Math.random() * 100) < 1 + round * 0.5)) {
+          if (!enemyBullets.some(bullet => ([enemyX + x * 6, enemyX + x * 6 + 1, enemyX + x * 6 + 2].contains(bullet[0])) && 
+            ([enemyY + y * 6 + 2, enemyY + y * 6 + 3, enemyY + y * 6 + 4].contains(bullet[1])))
+             ) {
+            enemyBullets.push([enemyX + x * 6, enemyY + y * 6 + 2]);
           }
         }
       }
-    }
-  }
-
-
-  // --- Collisions ---
-  
-  // Enemy bullets
-  for (let i = enemyBullets.length - 1; i > -1; i--) {
-    if (enemyBullets[i][0] === playerX && enemyBullets[i][1] === playerY) {
-      health -= 1;
-      enemyBullets.splice(i, 1);
-    }
-  }
-
-  // Player bullets
-  for (let i = playerBullets.length - 1; i > -1; i--)  {
-    for (let y = 0; y < enemies.length; y++) {
-      for (let x = 0; x < enemies[y].length; x++) {
-        if (enemies[y][x] === 1 &&
-            playerBullets[i][0] === enemyX + x &&
-            playerBullets[i][1] === enemyY + y) {
-          enemies[y][x] = 0;
-          playerBullets.splice(i, 1);
-          score += 10;
-        }
-      }
-    }
-  }
-
-  // Between bullets
-  for (let i = playerBullets.length - 1; i > -1; i--) {
-    for (let x = enemyBullets.length - 1; x > -1; x--) {
-      if (
-        playerBullets[i][0] === enemyBullets[x][0] &&
-        playerBullets[i][1] === enemyBullets[x][1] 
-      ) {
-        playerBullets.splice(i, 1);
-        enemyBullets.splice(x, 1);
-        break;
-      }
-    }
-  }
-  
-  
-  // --- Delete bullets ---
-  for (let i = enemyBullets.length - 1; i > -1; i--) {
-    if (enemyBullets[i][1] > canvas.height / box - 1) {
-      enemyBullets.splice(i, 1);
-    }
-  }
-
-  for (let i = playerBullets.length - 1; i > -1; i--) {
-    if (playerBullets[i][1] < 0) {
-      playerBullets.splice(i, 1);
     }
   }
 
@@ -316,7 +332,7 @@ function drawFrame() {
     for (let x = 0; x < enemies[y].length; x++) {
       if (enemies[y][x] === 1) {
         ctx.fillStyle = 'rgb(255, 0, 0)';
-        ctx.fillRect((enemyX + x) * box, (enemyY + y) * box, box, box);
+        ctx.fillRect((enemyX + x * 6) * box, (enemyY + y * 6) * box, box * 3, box * 3);
       }
     }
   }
@@ -324,42 +340,42 @@ function drawFrame() {
   // Enemy Bullets
   for (let i = 0; i < enemyBullets.length; i++) {
     ctx.fillStyle = 'rgb(255, 100, 0)';
-    ctx.fillRect(enemyBullets[i][0] * box + smallBox, enemyBullets[i][1] * box, smallBox, smallBox * 2);
+    ctx.fillRect(enemyBullets[i][0] * box, enemyBullets[i][1] * box, box, box * 2);
   }
 
   // Player
   ctx.fillStyle = 'rgb(50, 120, 250)';
-  ctx.fillRect(playerX * box, playerY * box, box, box);
+  ctx.fillRect(playerX * box, playerY * box, box * 3, box * 3);
 
   // Player Bullets
   for (let i = 0; i < playerBullets.length; i++) {
     ctx.fillStyle = 'rgb(255, 255, 255)';
-    ctx.fillRect(playerBullets[i][0] * box + smallBox, playerBullets[i][1] * box + smallBox, smallBox, smallBox * 2);
+    ctx.fillRect(playerBullets[i][0] * box, playerBullets[i][1] * box, box, box * 2);
   }
 
   // Health
   for (let i = 0; i < health - 1; i++) {
     ctx.fillStyle = 'rgb(50, 120, 250)';
-    ctx.fillRect(2 * box * i + box, canvas.height - 2 * box, box, box);
+    ctx.fillRect(6 * box * i + box * 3, canvas.height - 6 * box, box, box);
   }
 
   // Player can shoot
   if (playerShotTimer < 1) {
     ctx.fillStyle = 'rgb(255, 255, 255)';
     ctx.beginPath();
-    ctx.arc(canvas.width - 1.5 * box, canvas.height - 1.5 * box, Math.round(box / 2), 0, Math.PI * 2);
+    ctx.arc(canvas.width - 1.5 * box * 3, canvas.height - 1.5 * box * 3, Math.round(1.5 * box), 0, Math.PI * 2);
     ctx.fill();
   }
 
   // Red Line
   ctx.fillStyle = 'rgb(255, 0, 0)';
-  ctx.fillRect(0, canvas.height - 8 * box, canvas.width, 2);
+  ctx.fillRect(0, canvas.height - 24 * box, canvas.width, 2);
 
   
   // --- End Game ---
   for (let y = 0; y < enemies.length; y++) {
     for (let x = 0; x < enemies[y].length; x++) {
-      if (enemies[y][x] === 1 && enemyY + y > canvas.height / box - 8) {
+      if (enemies[y][x] === 1 && enemyY + y * 6 > canvas.height / box - 24) {
         endGame = true;
       }
     }
