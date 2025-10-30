03336a9148bd16c59f578283fd26be680ab3f842
aportermagee <aportermagee@gmail.com>
2025-10-30 12:11:55 -0400

    Center score text on canvas

--- spaceInvaders.js
+++ spaceInvaders.js
@@ -467,7 +467,8 @@ function drawFrame() {
   // Score
   ctx.font = '15px Arial';
   ctx.fillStyle = 'rgb(255, 255, 255)';
-  ctx.fillText('Score: ' + score, canvas.width / 2 - 45, canvas.height - 1.5 * box * 3);
+  let scoreText = 'Score: ' + score;
+  ctx.fillText(scoreText, canvas.width / 2 - (scoreText.length / 2), canvas.height - 1.5 * box * 3);
 
   
   // --- End Game ---
