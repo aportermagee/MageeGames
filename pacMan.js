117e21d33f6da8c6f109017f25306fd5b34e7536
aportermagee <aportermagee@gmail.com>
2025-11-04 12:21:36 -0500

    Implement user login check and initialize Supabase

--- pacMan.js
+++ pacMan.js
@@ -1,1 +1,20 @@
+// --- Return User To Login Page ---
+function isLoggedIn() {
+  return (localStorage.getItem('loggedIn') === 'true');
+}
+
+if (!(isLoggedIn())) {
+  window.location.href = 'index.html';
+}
+
+
+// --- Initialize Supabase ---
+const SUPABASE_URL = 'https://crvmgootjfbqkokrwsuu.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNydm1nb290amZicWtva3J3c3V1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4MzkyNTQsImV4cCI6MjA3NjQxNTI1NH0.Em26tIW4z2ulfRePTOVhkCmcMGOa0OOjBqC3kPJ-LpU';
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+
+// --- Set Up ---
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
 
