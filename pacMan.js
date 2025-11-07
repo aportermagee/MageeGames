259473977574fd70ad5d001568e186da238a4111
aportermagee <aportermagee@gmail.com>
2025-11-07 10:42:54 -0500

    Fix constructor typo in Ghost class

--- pacMan.js
+++ pacMan.js
@@ -48,7 +48,7 @@ async function updateHighScore() {
 
 // --- Ghost Class ---
 class Ghost {
-  constuctor (x, y, color) {
+  constructor (x, y, color) {
     this.x = x;
     this.y = y;
     this.color = color;
