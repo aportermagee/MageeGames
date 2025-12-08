fefee9052ce9cace6e422de20ee7f68e0689ca22
aportermagee <aportermagee@gmail.com>
2025-12-08 12:13:08 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,3 +1,7 @@
+if (localStorage.getItem('loggedIn') == true) {
+  window.location.href = 'home';
+}
+
 // --- Initialize Supabase ---
 const SUPABASE_URL = 'https://crvmgootjfbqkokrwsuu.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNydm1nb290amZicWtva3J3c3V1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4MzkyNTQsImV4cCI6MjA3NjQxNTI1NH0.Em26tIW4z2ulfRePTOVhkCmcMGOa0OOjBqC3kPJ-LpU';
