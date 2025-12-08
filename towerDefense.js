89811086a64b9abb1cae7ab2237f8fa432b274d8
aportermagee <aportermagee@gmail.com>
2025-12-08 17:31:52 -0500

    Update towerDefense.js

--- towerDefense.js
+++ towerDefense.js
@@ -1,1 +1,56 @@
+// --- Return User To Login Page ---
+function isLoggedIn() {
+  return (localStorage.getItem('loggedIn') === 'true');
+}
 
+if (!isLoggedIn()) {
+  window.location.href = 'index.html';
+}
+
+// --- Initialize Supabase ---
+const SUPABASE_URL = 'https://crvmgootjfbqkokrwsuu.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNydm1nb290amZicWtva3J3c3V1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4MzkyNTQsImV4cCI6MjA3NjQxNTI1NH0.Em26tIW4z2ulfRePTOVhkCmcMGOa0OOjBqC3kPJ-LpU';
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// --- High Score ---
+let highScore;
+
+async function getHighScore() {
+  const { data, error } = await supabaseClient
+    .from('HighScores')
+    .select('highScorePacMan')
+    .eq('id', JSON.parse(localStorage.getItem('user')).id)
+    .single();
+
+  if (error) {
+    console.error(error);
+  } else {
+    highScore = data.highScorePacMan;
+  }
+}
+
+getHighScore().then(function() {
+  document.getElementById('score').textContent = 'Score: 0 | High Score: ' + highScore;
+});
+
+async function updateHighScore() {
+  const { data, error } = await supabaseClient
+    .from('HighScores')
+    .update({ highScorePacMan: highScore })
+    .eq('id', JSON.parse(localStorage.getItem('user')).id);
+
+  if (error) {
+    console.error(error);
+  }
+}
+
+// --- Classes ---
+
+// --- Functions ---
+
+// --- Variables ---
+const scoreP = document.getElementById('scoreP');
+const startBtn = document.getElementById('startBtn');
+const homeBtn = document.getElementById('homeBtn');                                        
+  
+// --- Game Loops
