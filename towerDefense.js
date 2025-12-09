98cfe0e9660152126aab0d7794a33a22671c28fa
aportermagee <aportermagee@gmail.com>
2025-12-09 17:23:38 -0500

    Update towerDefense.js

--- towerDefense.js
+++ towerDefense.js
@@ -1,48 +1,11 @@
-// --- Return User To Login Page ---
-function isLoggedIn() {
-  return (localStorage.getItem('loggedIn') === 'true');
-}
-
-if (!isLoggedIn()) {
+// --- Set Up ---
+if (!localStorage.getItem('loggedIn') === 'true') {
   window.location.href = 'index.html';
 }
 
-// --- Initialize Supabase ---
 const SUPABASE_URL = 'https://crvmgootjfbqkokrwsuu.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNydm1nb290amZicWtva3J3c3V1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA4MzkyNTQsImV4cCI6MjA3NjQxNTI1NH0.Em26tIW4z2ulfRePTOVhkCmcMGOa0OOjBqC3kPJ-LpU';
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-// --- High Score ---
-let highScore;
-
-async function getHighScore() {
-  const { data, error } = await supabaseClient
-    .from('HighScores')
-    .select('highScorePacMan')
-    .eq('id', JSON.parse(localStorage.getItem('user')).id)
-    .single();
-
-  if (error) {
-    console.error(error);
-  } else {
-    highScore = data.highScorePacMan;
-  }
-}
-
-getHighScore().then(function() {
-  document.getElementById('score').textContent = 'Score: 0 | High Score: ' + highScore;
-});
-
-async function updateHighScore() {
-  const { data, error } = await supabaseClient
-    .from('HighScores')
-    .update({ highScorePacMan: highScore })
-    .eq('id', JSON.parse(localStorage.getItem('user')).id);
-
-  if (error) {
-    console.error(error);
-  }
-}
 
 // --- Classes ---
 class Grid {
@@ -58,7 +21,7 @@ class Grid {
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -74,23 +37,62 @@ class Grid {
 }
 
 // --- Functions ---
+async function getHighScore() {
+  const { data, error } = await supabaseClient
+    .from('HighScores')
+    .select('highScorePacMan')
+    .eq('id', JSON.parse(localStorage.getItem('user')).id)
+    .single();
+
+  if (error) {
+    console.error(error);
+  } else {
+    highScore = data.highScorePacMan;
+  }
+}
+
+
+async function updateHighScore() {
+  const { data, error } = await supabaseClient
+    .from('HighScores')
+    .update({ highScorePacMan: highScore })
+    .eq('id', JSON.parse(localStorage.getItem('user')).id);
+
+  if (error) {
+    console.error(error);
+  }
+}
 
 // --- Variables ---
-const canvas = document.getElementById('game');
-const ctx = canvas.getContext('2d');
-ctx.imageSmoothingEnabled = false;
+let html = {
+  canvas = document.getElementById('game'),
+  ctx = canvas.getContext('2d'),
+  score = document.getElementById('scoreP'),
+  startBtn = document.getElementById('startBtn'),
+  homeBtn = document.getElementById('homeBtn'),                                        
+};
 
-const scoreP = document.getElementById('scoreP');
-const startBtn = document.getElementById('startBtn');
-const homeBtn = document.getElementById('homeBtn');                                        
+let constants = { 
+  box = canvas.width / 20,
+  startPos = [0, 11],
+};
 
-const box = canvas.width / 20;
-  
-// --- Game Loops
+let game = {
+  highScore,
+};
+
+
+// --- Game Loops ---
 
 // --- Inputs ---
 document.addEventListener('keydown', event => {
   if (['ArrowUp', 'ArrowDown', 'ArrowRight', 'ArrowLeft'].includes(event.key)) {
     event.preventDefault();
   }
+});
+
+// --- Init ---
+html.ctx.imageSmoothingEnabled = false;
+getHighScore().then(function() {
+  document.getElementById('score').textContent = 'Score: 0 | High Score: ' + highScore;
 });
