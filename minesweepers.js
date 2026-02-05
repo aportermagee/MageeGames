913d67495d9a07e58a109c4bf44db0b13534cbef
aportermagee <aportermagee@gmail.com>
2026-02-05 12:15:46 -0500

    Implement board setup and mine generation
    Initialize game board and set up mine placement logic.

--- minesweepers.js
+++ minesweepers.js
@@ -1,3 +1,27 @@
+// --- Function ---
+function setBoard() {
+  game.mines = [];
+  game.board = [];
+
+  for (let i = 0; i < game.numMines; i++) {
+    const mine = [Math.round(Math.random() * 40), Math.round(Math.random() * 40)];
+    
+    if (game.mines.includes(mine)) {
+      i--;
+    } else {
+      game.mines.push(mine);
+    }
+  }
+
+  for
+}
+
+function click(event) {
+  const rect = html.canvas.getBoundingClientRect();
+  
+  const x = Math.abs(event.clientX - rect.left) / game.box;
+  const y = Math.abs(event.clientY - rect.top) / game.box;
+}
 
 // --- Variables ---
 const html = {
@@ -9,5 +33,11 @@ const html = {
 
 const game = {
   box: 10,
+  numMines: 10,
   mines: [],
+  board: [],
 };
+
+document.addEventListener('click', event => {
+  click(event);
+});
