8431e177fbf5b12aa4bf28ce2ad5fb3932e096ce
aportermagee <aportermagee@gmail.com>
2026-02-06 12:29:12 -0500

    Adjust minefield size and update game logic

--- minesweepers.js
+++ minesweepers.js
@@ -9,7 +9,7 @@ function setBoard() {
   ];
 
   for (let i = 0; i < game.numMines; i++) {
-    const mine = [Math.round(Math.random() * 40), Math.round(Math.random() * 40)];
+    const mine = [Math.round(Math.random() * 20), Math.round(Math.random() * 20)];
     
     if (game.mines.includes(mine)) {
       i--;
@@ -18,19 +18,19 @@ function setBoard() {
     }
   }
 
-  for (let i = 0; i < 40; i++) {
+  for (let i = 0; i < 20; i++) {
     game.board.push([]);
 
-    for (let j = 0; j < 40; j++) {
+    for (let j = 0; j < 20; j++) {
       game.board[i].push(0);
     }
   }
 
-  for (let i = 0; i < numMines; i++) {
+  for (let i = 0; i < game.numMines; i++) {
     game.board[game.mines[i]] = -1;
 
     for (let j = 0; j < pos.length; j++) {
-      if (game.mines[i][0] + pos[j][0] < 40 && game.mines[i][0] + pos[j][0] >= 0 && game.mines[i][1] + pos[j][1] < 40 && game.mines[i][1] + pos[j][1] >= 0) {
+      if (game.mines[i][0] + pos[j][0] < 20 && game.mines[i][0] + pos[j][0] >= 0 && game.mines[i][1] + pos[j][1] < 20 && game.mines[i][1] + pos[j][1] >= 0) {
         if (game.board[game.mines[i][1] + pos[j][1]][game.mines[i][0] + pos[j][0]] != -1) {
           game.board[game.mines[i][1] + pos[j][1]][game.mines[i][0] + pos[j][0]] += 1;
         }
@@ -41,22 +41,22 @@ function setBoard() {
 
 function draw() {
   html.ctx.fillStyle = 'rgb(0, 0, 0)';
-  html.ctx.fillRect(0, 0, canvas.width, canvas.height);
+  html.ctx.fillRect(0, 0, html.canvas.width, html.canvas.height);
 
   for (let i = 0; i < game.board.length; i++) {
     for (let j = 0; j < game.board[i].length; j++) {
-      if (!game.revealed.includes([i, j])) {
+      if (!game.revealed.includes([j, i])) {
         html.ctx.fillStyle = 'rgb(50, 50, 50)';
-        html.ctx.fillRect(j * box + 1, i * box + 1, box - 2, box - 2);
+        html.ctx.fillRect(j * game.box + 1, i * game.box + 1, game.box - 2, game.box - 2);
       } else {
         if (game.board[i][j] === -1) {
           html.ctx.fillStyle = 'rgb(0, 0, 150)';
-          html.ctx.fillRect(j * box, i * box, box, box);
+          html.ctx.fillRect(j * game.box, i * game.box, game.box, game.box);
         } else {
           html.ctx.font = '16px Roboto';
           html.ctx.fillStyle = 'rgb(200, 200, 200)';
           html.ctx.textAlign = 'center';
-          html.ctx.fillText('' + game.board[i][j], j * box + box / 2, i * box + box / 2);
+          html.ctx.fillText('' + game.board[i][j], j * game.box + game.box / 2, i * game.box + game.box / 2);
         }
       }
     }
@@ -79,11 +79,11 @@ const html = {
 };
 
 const game = {
-  box: 10,
+  box: 20,
   numMines: 10,
   mines: [],
   board: [],
-  revealed: [];
+  revealed: [[1, 1]],
 };
 
 document.addEventListener('click', event => {
